346dde5206bc29479b13c519a709a9304d49f5d8
Michael Bizzle <mbizzle@studiomoviegrill.com>
2018-05-11 20:43:52 -0500

    Created passport login functionality for sign up and login.

--- server.js
+++ server.js
@@ -6,6 +6,10 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
+var passport = require('passport');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
 // Sets up the Express App
 // =============================================================
@@ -15,10 +19,31 @@ var PORT = process.env.PORT || 8080;
 // Requiring our models for syncing
 var db = require("./models");
 
+// required for passport
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+require('./config/passport')(app);
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+})
+
+// Handlebars
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
 // Sets up the Express app to handle data parsing
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser('mysecret')); // read cookies (needed for auth)
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 // parse application/json
 app.use(bodyParser.json());
 
@@ -27,12 +52,13 @@ app.use(express.static("public"));
 
 // Routes
 // =============================================================
-require("./routes/api-routes.js")(app);
+var domRouter = require('./controllers/dom-controllers.js');
+app.use('/', domRouter);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
 });
