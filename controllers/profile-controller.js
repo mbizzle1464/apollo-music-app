fda4f38b4ed6649720bbef629715e62dc6a1d9ab
Shamil Arefin <landoflitany@gmail.com>
2018-05-15 20:53:20 -0500

    uWu profile page stuff

---
+++ controllers/profile-controller.js
@@ -0,0 +1,4 @@
+var path = require("path");
+var router = require('express').Router();
+var db = require("../models");
+
--- routes/auth.js
+++ routes/auth.js
@@ -10,6 +10,8 @@ module.exports = function (app, passport) {
 
     app.get('/signin', authController.signin);
 
+    app.get('/profile', authController.signin);
+
 
     app.post('/signup', passport.authenticate('local-signup', {
         successRedirect: '/dashboard',
