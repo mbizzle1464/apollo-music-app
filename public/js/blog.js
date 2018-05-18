688a7b3e4d89b7d3c6c48bf20c9c5fab56e41982
Michael Bizzle <mbizzle@studiomoviegrill.com>
2018-05-17 19:40:01 -0500

    Cleaned up Public Javascript files.

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,100 +1,97 @@
-// Need to clean this up. 
-
-
-$(document).ready(function() {
-  /* global moment */
+//Still need to work on this file. 
+$(document).ready(function () {
 
   // blogContainer holds all of our posts
   var blogContainer = $(".blog-container");
   var authorList = $("tbody");
   var postCategorySelect = $("#category");
- 
+
   // Click events for the edit and delete buttons
   $(document).on("click", "button.delete", handlePostDelete);
   $(document).on("click", "button.edit", handlePostEdit);
 
 
-   // The code below handles the case where we want to get blog posts for a specific user
-   // Looks for a query param in the url for user_id
-   var url = window.location.search;
-   var authorId;
-   if (url.indexOf("?author_id=") !== -1) {
-     authorId = url.split("=")[1];
-     getPosts(authorId);
-   }
-   // If there's no userId we just get all posts as usual
-   else {
-     getPosts();
-     getAuthor();
-   }
+  // The code below handles the case where we want to get blog posts for a specific user
+  // Looks for a query param in the url for user_id
+  var url = window.location.search;
+  var authorId;
+  if (url.indexOf("?author_id=") !== -1) {
+    authorId = url.split("=")[1];
+    getPosts(authorId);
+  }
+  // If there's no userId we just get all posts as usual
+  else {
+    getPosts();
+    getAuthor();
+  }
 
-   function getAuthor(author) {
-     $.get("/api/authors").done(function (data) {
-       console.log("authors", data);
-     });
-   }
+  function getAuthor(author) {
+    $.get("/api/authors").done(function (data) {
+      console.log("authors", data);
+    });
+  }
 
-   function getPosts(author) {
-     authorId = author || "";
-     if (authorId) {
-       authorId = "/?author_id=" + authorId;
-     }
-     $.get("/api/posts" + authorId, function (data) {
-       console.log("Posts", data);
-       posts = data;
-       if (!posts || !posts.length) {
-         displayEmpty(author);
-       } else {
-         initializeRows();
-       }
-     });
-   }
-
-    // InitializeRows handles appending all of our constructed post HTML inside blogContainer
-    function initializeRows() {
-      blogContainer.empty();
-      var postsToAdd = [];
-      for (var i = 0; i < posts.length; i++) {
-        postsToAdd.push(createNewRow(posts[i]));
+  function getPosts(author) {
+    authorId = author || "";
+    if (authorId) {
+      authorId = "/?author_id=" + authorId;
+    }
+    $.get("/api/posts" + authorId, function (data) {
+      console.log("Posts", data);
+      posts = data;
+      if (!posts || !posts.length) {
+        displayEmpty(author);
+      } else {
+        initializeRows();
       }
-      renderPosts(postsToAdd);
+    });
+  }
+
+  // InitializeRows handles appending all of our constructed post HTML inside blogContainer
+  function initializeRows() {
+    blogContainer.empty();
+    var postsToAdd = [];
+    for (var i = 0; i < posts.length; i++) {
+      postsToAdd.push(createNewRow(posts[i]));
     }
+    renderPosts(postsToAdd);
+  }
 
-     function renderPosts(rows) {
-       authorList.children().not(":last").remove();
-       blogContainer.children(".alert").remove();
-         console.log(rows);
-         authorList.prepend(rows);
-     }
+  function renderPosts(rows) {
+    authorList.children().not(":last").remove();
+    blogContainer.children(".alert").remove();
+    console.log(rows);
+    authorList.prepend(rows);
+  }
 
-    // This function constructs a post's HTML
-    function createNewRow(post) {
-      console.log(post);
-      var newTr = $("<tr>");
-      newTr.addClass("Entries");  
-       var formattedDate = new Date(post.createdAt);
-       formattedDate = moment(formattedDate).format("MMMM Do YYYY, h:mm:ss a");
-       var authorName;
-      authorName = post.Author.firstname + " " + post.Author.lastname;  
+  // This function constructs a post's HTML
+  function createNewRow(post) {
+    console.log(post);
+    var newTr = $("<tr>");
+    newTr.addClass("Entries");
+    var formattedDate = new Date(post.createdAt);
+    formattedDate = moment(formattedDate).format("MMMM Do YYYY, h:mm:ss a");
+    var authorName;
+    authorName = post.Author.firstname + " " + post.Author.lastname;
 
-       newTr.data("author", post);
-       newTr.append("<tr><th class='authorName'>" + authorName + "</th></tr>");
-       newTr.append("<tr><td class='authorUserName'><a href='/cms?author_id=" + post.AuthorId + "'> @" + post.Author.username + "</a></td></tr>");
-       newTr.append("<tr><td class='post'>" + post.body + "</td></tr>");
-       newTr.append("<tr><td class='date'>" + formattedDate + "</td></tr>");
-       return newTr;
-    }
+    newTr.data("author", post);
+    newTr.append("<tr><th class='authorName'>" + authorName + "</th></tr>");
+    newTr.append("<tr><td class='authorUserName'><a href='/cms?author_id=" + post.AuthorId + "'> @" + post.Author.username + "</a></td></tr>");
+    newTr.append("<tr><td class='post'>" + post.body + "</td></tr>");
+    newTr.append("<tr><td class='date'>" + formattedDate + "</td></tr>");
+    return newTr;
+  }
 
   // This function does an API call to delete posts
   function deletePost(id) {
     $.ajax({
-      method: "DELETE",
-      url: "/api/posts/" + id
-    })
-    .then(function() {
-      alert('post deleted!');
-      $(`[data-post=${id}]`).remove();
-    });
+        method: "DELETE",
+        url: "/api/posts/" + id
+      })
+      .then(function () {
+        alert('post deleted!');
+        $(`[data-post=${id}]`).remove();
+      });
   }
 
   // This function figures out which post we want to delete and then calls deletePost
@@ -122,12 +119,16 @@ $(document).ready(function() {
     }
     blogContainer.empty();
     var messageh2 = $("<h2>");
-    messageh2.css({ "text-align": "center", "margin-top": "50px" });
+    messageh2.css({
+      "text-align": "center",
+      "margin-top": "50px"
+    });
     messageh2.html("No posts yet" + partial + ", navigate <a href='/cms" + query +
-    "'>here</a> in order to get started.");
+      "'>here</a> in order to get started.");
     blogContainer.append(messageh2);
   }
 
 
 });
+
 
--- public/js/cms.js
+++ public/js/cms.js
@@ -1,8 +1,4 @@
-// Currently working on this section
-
-
-
-$(document).ready(function() {
+$(document).ready(function () {
   var bodyInput = $("#body");
   var cmsForm = $("#cms");
   $(cmsForm).on("submit", handleFormSubmit);
@@ -11,21 +7,15 @@ $(document).ready(function() {
   var authorId;
   var updating = false;
 
-  // If we have this section in our url, we pull out the post id from the url
-  // In '?post_id=1', postId is 1
   if (url.indexOf("?post_id=") !== -1) {
     postId = url.split("=")[1];
     getPostData(postId, "post");
-  }
-  // Otherwise if we have an author_id in our url, preset the author select box to be our Author
-  else if (url.indexOf("?author_id=") !== -1) {
+  } else if (url.indexOf("?author_id=") !== -1) {
     authorId = url.split("=")[1];
   }
 
-  // Getting the authors, and their posts
   getAuthors();
 
-    // A function to get Authors and then render our list of Authors
   function getAuthors() {
     $.get("/api/authors").done(function (data) {
       console.log("authors", data);
@@ -33,40 +23,41 @@ $(document).ready(function() {
   }
 
 
-  // A function for handling what happens when the form to create a new post is submitted
   function handleFormSubmit(event) {
+    $.get("/api/authors", function (data) {
+      var rowsToAdd = [];
+      for (var i = 0; i < data.length; i++) {
+        //console.log(data[i]);  
+        authorId = data[i].id;
+        console.log(authorId);
+      }
+    });
     event.preventDefault();
-    // Wont submit the post if we are missing a body, title, or author
     if (!bodyInput.val().trim()) {
       return;
     }
-    // Constructing a newPost object to hand to the database
     var newPost = {
       body: bodyInput
         .val()
         .trim(),
-      AuthorId: authorSelect.val()
+      AuthorId: authorId
     };
+    console.log(newPost);
 
-    // If we're updating a post run updatePost to update a post
-    // Otherwise run submitPost to create a whole new post
     if (updating) {
       newPost.id = postId;
       updatePost(newPost);
-    }
-    else {
+    } else {
       submitPost(newPost);
     }
   }
 
-  // Submits a new post and brings user to blog page upon completion
   function submitPost(post) {
-    $.post("/api/posts", post, function() {
+    $.post("/api/posts", post, function () {
       window.location.href = "/dashboard";
     });
   }
 
-  // Gets post data for the current post if we're editing, or if we're adding to an author's existing posts
   function getPostData(id, type) {
     var queryUrl;
     switch (type) {
@@ -79,38 +70,26 @@ $(document).ready(function() {
       default:
         return;
     }
-    $.get(queryUrl, function(data) {
+    $.get(queryUrl, function (data) {
       if (data) {
         console.log(data.AuthorId || data.id)
-        // If this post exists, prefill our cms forms with its data
-        titleInput.val(data.title);
         bodyInput.val(data.body);
         authorId = data.AuthorId || data.id;
-        // If we have a post with this id, set a flag for us to know to update the post
-        // when we hit submit
         updating = true;
       }
     });
   }
 
-  // Creates the author options in the dropdown
-  function createAuthorRow(author) {
-    var listOption = $("<option>");
-    listOption.attr("value", author.id);
-    listOption.text(author.firstname + " " + author.lastname);
-    return listOption;
-  }
 
-  // Update a given post, bring user to the blog page when done
   function updatePost(post) {
     $.ajax({
-      method: "PUT",
-      url: "/api/posts",
-      data: post
-    })
-    .then(function() {
-      window.location.href = "/dashboard";
-    });
+        method: "PUT",
+        url: "/api/posts",
+        data: post
+      })
+      .then(function () {
+        window.location.href = "/dashboard";
+      });
   }
 
   function getAuthor(author) {
@@ -121,7 +100,7 @@ $(document).ready(function() {
     $.get("/api/cms" + authorId, function (data) {
       console.log(data);
     });
-}
+  }
 
   function upsertAuthor(authorData) {
     console.log(authorData)
